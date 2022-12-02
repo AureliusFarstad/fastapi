dd78a9b5aaf099eb7b2e665dbb8da6b4a224f0e2
Marcus Farstad <marcusfarstad@protonmail.ch>
2022-12-02 18:21:51 +0100

    new conf

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,7 +1,7 @@
 from multiprocessing import cpu_count
 
 # Socket Path
-# bind = 'unix:/home/demo/fastapi_demo/gunicorn.sock'
+bind = 'unix:/home/deployer/fastapi/gunicorn.sock'
 
 # Worker Options
 workers = cpu_count() + 1
